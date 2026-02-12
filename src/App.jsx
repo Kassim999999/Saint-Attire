bec6d2b6f14a00da8149a9d9cb72afe19105486c
Kassim <kassimrooney@gmail.com>
2026-02-13 01:20:19 +0300

    Create Cart context

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Drop from "./pages/Drop";
+
+import "./index.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/drop" element={<Drop />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
