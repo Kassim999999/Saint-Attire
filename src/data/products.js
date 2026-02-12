3addd4de6299f4bba57cb01ad4838cf85eee4040
Kassim <kassimrooney@gmail.com>
2026-02-12 13:56:58 +0300

    create mock product data

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: 1,
+    name: "FLY PANT 001",
+    price: 4500,
+    image: "/pant1.jpg",
+    description: "Relaxed street cut. Abstract panel print. Limited release."
+  },
+  {
+    id: 2,
+    name: "FLY JORT 001",
+    price: 3200,
+    image: "/jort1.jpg",
+    description: "Loose fit. Graphic-heavy design. Built for movement."
+  }
+];
