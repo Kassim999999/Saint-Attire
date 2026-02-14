0ed29b0cac2fd0d7833898f895cc5693928b9c76
Kassim <kassimrooney@gmail.com>
2026-02-14 11:11:02 +0300

    Update checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import { useCart } from "../context/CartContext";
+import "../styles/Checkout.css"
 
 export default function Checkout() {
   const { cart, clearCart } = useCart();
+  const [form, setForm] = useState({
+    name: "",
+    phone: "",
+    address: ""
+  });
 
-  const handleOrder = () => {
-    alert("Payment integration goes here (Flutterwave)");
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Order:", { cart, form });
+    alert("Payment integration next");
     clearCart();
   };
 
   return (
     <div className="checkout-page">
       <h1>CHECKOUT</h1>
-      <p>Total Items: {cart.length}</p>
-      <button onClick={handleOrder}>
-        PAY NOW
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input name="name" placeholder="Full Name" onChange={handleChange} required />
+        <input name="phone" placeholder="Phone Number" onChange={handleChange} required />
+        <input name="address" placeholder="Shipping Address" onChange={handleChange} required />
+        <button type="submit">PROCEED TO PAYMENT</button>
+      </form>
     </div>
   );
 }
