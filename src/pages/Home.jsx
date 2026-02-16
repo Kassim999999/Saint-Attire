55cc7bb86805c28341d9e99d9e4bc956c862e1b4
Kassim <kassimrooney@gmail.com>
2026-02-16 22:48:27 +0300

    Update Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout"
+import { Link } from "react-router-dom"
 import "../styles/Home.css"
 
 export default function Home() {
   return (
-    <div className="hero">
-      <div className="hero-content">
-        <h1>FLY SEASON</h1>
-        <p>MOVE DIFFERENT.</p>
+    <MainLayout>
+      <section className="hero">
+
+        <h1 className="hero-title">
+          FLY<br />SEASON
+        </h1>
+
+        <p className="hero-sub">
+          Graphic-heavy urban wear.
+          <br />
+          Built different.
+        </p>
+
         <Link to="/drop" className="hero-btn">
-          SHOP DROP 01
+          ENTER DROP 01
         </Link>
-      </div>
-    </div>
-  );
+
+      </section>
+    </MainLayout>
+  )
 }
