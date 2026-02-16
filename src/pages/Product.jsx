d6f4eb9091b0c4ada77f1944f76de3d39b26de29
Kassim <kassimrooney@gmail.com>
2026-02-16 22:51:52 +0300

    Update Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,47 +1,41 @@
-import { useParams } from "react-router-dom";
-import { products } from "../data/products";
-import { useCart } from "../context/CartContext";
-import { useState } from "react";
-
+import MainLayout from "../layouts/MainLayout"
+import "../styles/Product.css"
 
 export default function Product() {
-  const { id } = useParams();
-  const product = products.find((p) => p.id === parseInt(id));
-  const { addToCart } = useCart();
-  const [selectedSize, setSelectedSize] = useState("");
-
-  if (!product) return <h2>Product Not Found</h2>;
-
   return (
-    <div className="product-page">
-      <img src={product.image} alt={product.name} />
-      <div>
-        <h1>{product.name}</h1>
-        <p>KES {product.price}</p>
-        <p>{product.stock} pieces left</p>
-        <p>{product.description}</p>
+    <MainLayout>
+      <div className="product-page">
 
-        <div className="sizes">
-  {product.sizes.map((size) => (
-    <button
-      key={size}
-      className={selectedSize === size ? "active" : ""}
-      onClick={() => setSelectedSize(size)}
-    >
-      {size}
-    </button>
-  ))}
-</div>
+        {/* LEFT SIDE - IMAGE */}
+        <div className="product-image">
+          <img src="/pants1.jpg" alt="Studded Black Pants" />
+        </div>
 
-<button
-  onClick={() => {
-    if (!selectedSize) return alert("Select size");
-    addToCart({ ...product, size: selectedSize });
-  }}
->
-  ADD TO CART
-</button>
+        {/* RIGHT SIDE - INFO */}
+        <div className="product-info">
+          <h1>STUDDED BLACK PANTS</h1>
+
+          <p className="product-price">KSH 950</p>
+
+          <p className="product-desc">
+            Heavy graphic urban pants. Built loud. 
+            Structured fit with statement detailing.
+          </p>
+
+          <div className="size-selector">
+            <button>S</button>
+            <button>M</button>
+            <button>L</button>
+            <button>XL</button>
+          </div>
+
+          <button className="add-cart-btn">
+            ADD TO CART
+          </button>
+
+        </div>
+
       </div>
-    </div>
-  );
+    </MainLayout>
+  )
 }
